518dbfd40fde8f876312737eeb7a1d12495a54a8
Christoph Dorn <christoph@christophdorn.com>
2015-07-27 23:46:03 -0700

    better routing after first real use

--- server.js
+++ server.js
@@ -1,18 +1,4 @@
 
-/*
-var http = require('http');
-
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  res.end('Hello World from proxy.pinf.io\n');
-
-}).listen(15000, process.env.OPENSHIFT_INTERNAL_IP);
-*/
-
-console.log("io.pinf.proxy server loading");
-
 const HTTP = require('http');
 const HTTP_HASH_ROUTER = require('http-hash-router');
 const STACK = require('stack');
@@ -34,21 +20,74 @@ console.log("API.config", API.config);
 
 		var api = API[API.config.services[route].implementation];
 
+		if (
+			!api ||
+			typeof api !== "object"
+		) {
+			console.error("API.__pgl_meta", API.__pgl_meta);
+			console.error("API.config.services['" + route + "'].implementation:", api);
+			throw new Error("Module at API['" + API.config.services[route].implementation + "'] does not export api!");
+		}
+
 		// We should support all the popular low-level adapters here.
 
 		// @see https://nodejs.org/api/http.html#http_event_request
 		if (api["nodejs.org/api/http/0"]) {
-			router.set("/" + route + "/*", api["nodejs.org/api/http/0"]);
+
+			function handler1 (req, res, opts, callback) {
+
+				req.urlPrefix = req.url;
+				req.url = "/" + opts.splat;
+				req.urlPrefix = req.urlPrefix.substring(0, req.urlPrefix.indexOf(req.url));
+
+				try {
+					api["nodejs.org/api/http/0"](req, res);
+				} catch (err) {
+					return callback(err);
+				}
+			}
+
+			router.set("/" + route + "/", handler1);
+			router.set("/" + route + "/*", handler1);
 		} else
 		// @see https://github.com/creationix/stack
 		if (api["github.com/creationix/stack/0"]) {
-			router.set("/" + route + "/*", STACK(api["github.com/creationix/stack/0"]));
+
+			function handler2 (req, res, opts, callback) {
+
+				var urlParts = req.url.split("?");
+				req.urlPrefix = req.url;
+				if (opts.splat) {
+					req.url = "/" + opts.splat;
+					if (urlParts.length > 1) {
+						req.url += "?" + urlParts.slice(1).join("?");
+					}
+					req.urlPrefix = req.urlPrefix.substring(0, req.urlPrefix.lastIndexOf(req.url));
+				} else {
+					req.url = "/";
+					if (urlParts.length > 1) {
+						req.url += "?" + urlParts.slice(1).join("?");
+					}
+				}
+				req.urlPrefix = req.urlPrefix.replace(/\/$/, "");
+
+				try {
+					STACK(api["github.com/creationix/stack/0"])(req, res);
+				} catch (err) {
+					return callback(err);
+				}
+			}
+
+			router.set("/" + route + "/", handler2);
+			router.set("/" + route + "/*", handler2);
 		}
+
+		API.console.log("Service for route '" + route + "' available at: http://" + API.config.bind + ":" + API.config.port + "/" + route + "/");
 	}
 
 	HTTP.createServer(function handler (req, res) {
 
-		logger.info("Request", req);
+		logger.info("Request", req.url);
 
 	    router(req, res, {}, function (err) {
 	        if (err) {
@@ -62,8 +101,7 @@ console.log("API.config", API.config);
 		API.config.bind
 	);
 
-console.log("Server listening at: http://" + API.config.bind + ":" + API.config.port);
-
+	console.log("Server listening at: http://" + API.config.bind + ":" + API.config.port);
 
 
 /*
@@ -85,5 +123,3 @@ console.log("Server listening at: http://" + API.config.bind + ":" + API.config.
 
 
 });
-
-console.log("io.pinf.proxy server loaded");
